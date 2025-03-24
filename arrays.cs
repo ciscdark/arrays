f9ccf6360c571bfb66c2609e233ac1f30821be59
Ciscdark <cisco01010b@gmail.com>
2025-03-23 23:29:35 -0400

    Primer commit - Proyecto de C#

---
+++ arrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] empleados = new string[5];
+        string[] departamentos = new string[5];
+        string[] resultado = new string[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Ingrese el nombre del empleado: ");
+            empleados[i] = Console.ReadLine();
+            Console.Write("Ingrese el nombre del departamento: ");
+            departamentos[i] = Console.ReadLine();
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            resultado[i] = empleados[i] + " - " + departamentos[i];
+        }
+
+        Console.WriteLine("\nResultado:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(resultado[i]);
+        }
+    }
+}
